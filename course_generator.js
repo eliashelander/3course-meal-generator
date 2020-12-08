2dd614b60ea3632ce69caf9c3227175545994d8e
elias helander <elias@eliass-MacBook-Pro.local>
2020-12-08 13:19:14 -0500

    Planned project and added example data for dishes

---
+++ course_generator.js
@@ -0,0 +1,50 @@
+// Create 3 lists of dishes.
+// Starters:
+let starters = [
+    'Kakori Kebabs',
+    'Stir Fried Chilli Chicken',
+    'Microwave Paneer Tikkas',
+    'Aloo and Dal ki Tikki',
+    'Cheese Balls',
+    'Chicken Satay',
+    'Bhuna Masala Chicken Wings',
+    'Tangri Kebabs',
+    'Hot Basil Chicken Cups',
+    'Crumb-Fried Chicken'
+]
+
+// Mains:
+let mains = [
+    'Creamy Tomato Pasta',
+    'Baked Honey Mustard Chicken',
+    'Red and White Tortellini',
+    'Classic Meatloaf',
+    'Pot Roast',
+    'Crockpot Chicken and Dumplings',
+    'Honey Orange Fish Fillets',
+    'Meatball Stew Casserole',
+    'Vegan Skewers',
+    'Vietnamise Porkchops'
+]
+
+// Desserts:
+let desserts = [
+    'No Bake Kool-Aid Pie',
+    'Peanut Butter Stuffed Brownies',
+    'Monster Cookie No Bake Bars',
+    'Perfect Chocolate Chip Cookies',
+    'Peanut Butter Cup Dump Cake',
+    'Strawberry Cheesecake Lasagna',
+    'No Bake Mint Chocolate Chip Pie',
+    'Easy No Bake Avalanche Cookies',
+    'Gooey Brownie Pie',
+    'Reeses Fudge '
+]
+
+// Function to generate starter
+
+// Function to generate main
+
+// Function to generate dessert
+
+// Function to return complete 3 course meal
